0500020e42a188a39552d31551df524349f239c8
Emil-ceb <emiliocec@outlook.com>
2021-07-01 03:06:59 -0500

    Se creo el script para spawn aleatorio

--- Assets/Scripts/Ejercicio 3/Spawn.cs
+++ Assets/Scripts/Ejercicio 3/Spawn.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,15 +12,24 @@ Descripcion: Creacion de monstruos de manera aleatoria.
 
 public class Spawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] Mob;
+    float randX;
+    float randY;
+    Vector2 whereToSpawn;
+    public float spawnRate = 3f;
+    float nextSpawn = 0.0f;
+    int randomMob;
 
-    // Update is called once per frame
     void Update()
     {
+        if(Time.time > nextSpawn){
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range (-8.89f, 8.89f);
+            randY = Random.Range (-5f, 5f);
+            whereToSpawn = new Vector2 (randX, randY);
+            randomMob = Random.Range (0,Mob.Length);
+            Instantiate(Mob[randomMob], whereToSpawn,Quaternion.identity);
+        }
         
     }
 }
